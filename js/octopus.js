0c7d4247ef1d17831db5fa00a79748a6474e1b13
Diego Riera <rieradid@tcd.ie>
2017-06-29 17:43:22 +0200

    Add files via upload

--- js/octopus.js
+++ js/octopus.js
@@ -1,54 +1,52 @@
 var ViewModel = function() {
-    var self = this;
+	var self = this;
 
-    // Hard coded array simply converted in to a ko observable array, this will the reference array we wall as observable un ko, not the hard coded one
+	// Hard coded array simply converted in to a ko observable array, this will the reference array we wall as observable un ko, not the hard coded one
 
-    self.bruxellesLocs = ko.observableArray(myLocs);
+	self.bruxellesLocs = ko.observableArray(myLocs);
 
 
-    // we initialize the observable with an empty string, those will result in a log of everything that is typed by the user in the console.
-    self.userSearch = ko.observable('');
+	// we initialize the observable with an empty string, those will result in a log of everything that is typed by the user in the console.
+	self.userSearch = ko.observable('');
 
-    // this array will contain all the markers data after they 've been filtered into the userfilterlocs computed observable'
+	// this array will contain all the markers data after they 've been filtered into the userfilterlocs computed observable'
 
-    self.filteredlist = ko.observableArray([]);
+	self.filteredlist = ko.observableArray([]);
 
-    // this array will contain all the markers data after they 've been rejected from the filter into the userfilterlocs computed observable'
-    self.NotFilterList = ko.observableArray([]);
+	// this array will contain all the markers data after they 've been rejected from the filter into the userfilterlocs computed observable'
+	self.NotFilterList = ko.observableArray([]);
 
-    // This method create a Google Map event function (see Google API)
-    self.clickMarker = function(location) {
-        google.maps.event.trigger(location.marker, 'click');
-    }
+	// This method create a Google Map event function (see Google API)
+	self.clickMarker = function(location) {
+		google.maps.event.trigger(location.marker, 'click');
+	}
 
-    self.userFilteredLocs = ko.computed(function() {
+	self.userFilteredLocs = ko.computed(function() {
+		var input = self.userSearch().toLowerCase();
 
-        var input = self.userSearch().toLowerCase();
-
-        // We use a computed observable to create the searchfilter
-        if (input) { //If there is an input
-            self.filteredlist([]); // We empty filteredlist
-            self.NotFilterList([]); // We empty nofilteredlist
-            self.bruxellesLocs().forEach(function(loc, i) { // We use a loop element function
-                if (self.bruxellesLocs()[i].title.toLowerCase().indexOf(input) > -1) { // To check if there is an index match 
-                    self.filteredlist.push(loc); // If yes, we push it to the filteredlist array
-
-                    loc.marker.setVisible(true); // We use a setVisible buildin function to make them appear
-                } else {
-                    self.NotFilterList.push(loc); // If no, we push it to the nofilteredlist array
-                    loc.marker.setVisible(false); // We use a setVisible buildin function to make them dissapear
-                }
-            });
-
-            return self.filteredlist();
-
-        } else { //IS there is no input
-            self.NotFilterList().forEach(function(pos, i) { // We use a loop element function
-                pos.marker.setVisible(true); // And use a setVisible buildin function to make them appear
-            });
-            return self.bruxellesLocs();
-        }
-    });
+		// We use a computed observable to create the searchfilter
+		if (input) { //If there is an input
+			self.filteredlist([]); // We empty filteredlist
+			self.NotFilterList([]); // We empty nofilteredlist
+			self.bruxellesLocs().forEach(function(loc, i) { // We use a loop element function
+				if (self.bruxellesLocs()[i].title.toLowerCase().indexOf(input) > -1) { // To check if there is an index match 
+					self.filteredlist.push(loc); // If yes, we push it to the filteredlist array
+					loc.marker.setVisible(true); // We use a setVisible buildin function to make them appear
+				}
+				else {
+					self.NotFilterList.push(loc); // If no, we push it to the nofilteredlist array
+					loc.marker.setVisible(false); // We use a setVisible buildin function to make them dissapear
+				}
+			});
+			return self.filteredlist();
+		}
+		else { //IS there is no input
+			self.NotFilterList().forEach(function(pos, i) { // We use a loop element function
+				pos.marker.setVisible(true); // And use a setVisible buildin function to make them appear
+			});
+			return self.bruxellesLocs();
+		}
+	});
 
 };
 var vm = new ViewModel();
